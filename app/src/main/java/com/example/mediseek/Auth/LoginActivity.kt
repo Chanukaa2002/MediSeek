3056dbc6dd7fa97eb0987b438116b1367d508c5f
Chanuka Dilshan <mchanuka72@gmail.com>
2025-07-04 22:32:51 +0530

    bug(redirect) fix a bug to redirect login to client

--- app/src/main/java/com/example/mediseek/Auth/LoginActivity.kt
+++ app/src/main/java/com/example/mediseek/Auth/LoginActivity.kt
@@ -25,8 +25,6 @@ class LoginActivity : AppCompatActivity() {
             // Handle login logic here
             val intent = Intent(this,ClientActivity::class.java)
             startActivity(intent)
-            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
-            finish()
         }
 
         val googleLoginButton = findViewById<MaterialButton>(R.id.google_login_button)
